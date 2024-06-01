d9f9906f5efe1cc52621507947298adf9ee93d4b
halosij <135062575+halosij@users.noreply.github.com>
2024-06-01 12:10:09 +0200

    Added testing for connection to sql server database

--- server.js
+++ server.js
@@ -1,6 +1,31 @@
+const sql = require('mssql');
+
 const express = require('express');
 const app = express();
 const port = 3000;
+
+const connectionString = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:clouddbazure.database.windows.net,1433;Database=AzureCloudDB;Uid=AzureDemoWS01;Pwd=AzureDemo1409;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+
+async function connectToDatabase() {
+  try {
+    // Verbinde mit der Datenbank
+    await sql.connect(connectionString);
+    console.log('Verbindung zur SQL Server-Datenbank erfolgreich');
+
+    // Führe eine Abfrage aus
+    const result = await sql.query`SELECT * FROM test_table`;
+    const filteredResults = result.recordset.filter(row => row.id === 2);
+    let t = filteredResults[0].test
+    return t;
+    //return result;
+
+  } catch (err) {
+    console.error('Fehler bei der Verbindung:', err);
+  } finally {
+    // Schließe die Verbindung
+    await sql.close();
+  }
+}
 
 // CORS zulassen
 app.use((req, res, next) => {
@@ -11,7 +36,11 @@ app.use((req, res, next) => {
 
 // Route definieren, die "test" zurückgibt
 app.get('/api/test', (req, res) => {
-  res.send('test');
+  const result = connectToDatabase();
+  console.log(result);
+  res.send(result);
+  //res.send('test');
+  
 });
 
 app.listen(port, () => {
